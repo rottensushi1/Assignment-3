1d3b949c0e496276243a65d385c68addd2489d68
taiyo <tsuzuki7660@conestogac.on.ca>
2022-10-11 18:09:43 -0400

    Add project files.

---
+++ Item 1/Source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Taiyo Suzuki
+//Sept 23, 2022
+//PROG71985 - Assignment 3
+//Item 1: Write and test a function called SmallerOf() that replaces the contents of two double variables with the smaller of the two values. 
+//For example, SmallerOf(x, y) would reset both x and y to the smaller of the two. Be sure to test(and include) all the edge cases you can think of 
+//to ensure your function works for all double variables.
+
+int main(void)
+{
+
+
+	return 0;
+}
---
+++ Item 4/Source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Taiyo Suzuki
+//Sept 23, 2022
+//PROG71985 - Assignment 3
+//Item 4: Create a structure template with two members according to the following criteria:
+//	1. The first member is a student number as a single fixed length field following the format of 10 digits: YYYYMMwxyz 
+//		YYYY = birthyear, MM = birth month, wxyz = random 4 digit code).
+//	2. the second member is a structure with three members according to the following criteria:
+//		– ”first name”
+//		– ”middle name”
+//		– ”last name”
+//Write and test a program that creates and initializes an array of five of these structures (Please make up pretend names / numbers for your 5 students - 
+//be sure to have at least 2 students with middle names (and the full middle name must be entered) and at least 1 without any middle name. 
+//Write a single function that accepts the array of structures as a parameter for printing the array data in the following format:
+//2001041234 – LastName, FirstName MiddleInitial. Note that only the initial of the middle name should be printed, followed by a period.
+//Any of your students who lack a middle name will end the output line at the end of the first name.
+
+int main(void)
+{
+
+
+	return 0;
+}
